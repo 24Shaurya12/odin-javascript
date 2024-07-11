7259e974ea06b1cf265d2f77bafbb5fb059b33de
shaurya <shauryaagarwal2412@gmail.com>
2024-07-11 13:11:47 +0530

    Complete 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...checker) {
+    let returnArray = [];
+    for (eachElement of array) {
+        let present = false;
+        for (eachChecker of checker) {
+            if (eachChecker === eachElement) {
+                present = true;
+            }
+        }
+        if (present === false) {
+            returnArray.push(eachElement);
+        }
+    } 
+    return returnArray;
 };
+
+console.log(removeFromArray([1, 2, 3], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
